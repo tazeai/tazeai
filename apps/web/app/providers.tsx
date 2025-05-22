210dcb1e947864ae1398a0d91c4eb9237e754b90
Zack <lorenx@163.com>
2025-05-22 14:55:23 +0800

    feat: add analytics

--- apps/web/app/providers.tsx
+++ apps/web/app/providers.tsx
@@ -6,6 +6,12 @@ import { I18nProvider } from '@tazeai/i18n/provider';
 import { Toaster } from '@tazeai/ui/components/sonner';
 import { ThemeProvider, type UIProviderProps } from '@tazeai/ui/providers';
 import { useMemo, type ReactNode } from 'react';
+import { FpjsProvider } from '@fingerprintjs/fingerprintjs-pro-react';
+import { Analytics } from '@vercel/analytics/next';
+import Fingerprintjs from '@/components/fingerprintjs';
+
+const fpjsPublicApiKey = process.env.NEXT_PUBLIC_FPJS_PUBLIC_API_KEY as string;
+const isProduction = process.env.NODE_ENV === 'production';
 
 export interface ProvidersProps {
   children: ReactNode;
@@ -19,11 +25,23 @@ export function Providers({ children, themeProps, lang }: ProvidersProps) {
   }, [lang]);
 
   return (
-    <ThemeProvider {...themeProps}>
-      <I18nProvider settings={i18nSettings} resolver={i18nResolver}>
-        <Toaster />
-        {children}
-      </I18nProvider>
-    </ThemeProvider>
+    <FpjsProvider
+      loadOptions={{
+        apiKey: fpjsPublicApiKey,
+      }}
+    >
+      <ThemeProvider {...themeProps}>
+        <I18nProvider settings={i18nSettings} resolver={i18nResolver}>
+          <Toaster />
+          {children}
+          {isProduction && (
+            <>
+              <Fingerprintjs />
+              <Analytics />
+            </>
+          )}
+        </I18nProvider>
+      </ThemeProvider>
+    </FpjsProvider>
   );
 }
---
+++ apps/web/components/fingerprintjs.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useVisitorData } from '@fingerprintjs/fingerprintjs-pro-react';
+import { useEffect } from 'react';
+
+function Fingerprintjs() {
+  const { data } = useVisitorData(
+    { extendedResult: true },
+    { immediate: true },
+  );
+
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+    }
+  }, [data]);
+
+  return null;
+}
+
+export default Fingerprintjs;
