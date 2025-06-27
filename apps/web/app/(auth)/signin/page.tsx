929100cbe1791cd1e1f880c790b49c9bce19c89f
Zack <lorenx@163.com>
2025-06-27 12:42:51 +0800

    feat: add Splash component for fluid simulation
    - Introduced a new Splash component that utilizes WebGL for fluid simulation effects.
    - Implemented various shaders and rendering techniques to create dynamic visual effects.
    - Added event listeners for mouse and touch interactions to manipulate the fluid simulation in real-time.
    - Configurable parameters for simulation resolution, dye resolution, and other visual properties to enhance customization.

--- apps/web/app/(auth)/signin/page.tsx
+++ apps/web/app/(auth)/signin/page.tsx
@@ -4,12 +4,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { GuestGuard } from '../_components/guest-guard';
 import { SignInForm } from '../_components/signin-form';
+import { Splash } from '../_components/splash';
 
 export default function SignInPage() {
   return (
     <>
-      <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
-        <div className="flex w-full max-w-sm flex-col gap-6">
+      <div className="relative flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
+        <Splash />
+        <div className="flex w-full max-w-sm flex-col gap-6 z-10">
           <Link
             className="flex items-center gap-2 self-center font-medium"
             href="/"
